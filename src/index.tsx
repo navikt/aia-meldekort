cba3e40790fe2c033721c20263db8888fbd8c55c
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2024-04-10 10:27:29 +0200

    initsielt byggeoppsett

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import App from './app';
+import {createRoot} from "react-dom/client";
+
+const root = createRoot(document.getElementById('maincontent') as HTMLElement);
+root.render(<App/>);
---
+++ vite.config.js
@@ -0,0 +1,60 @@
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
+import {rollupImportMapPlugin} from 'rollup-plugin-import-map';
+import terser from '@rollup/plugin-terser';
+import svgr from 'vite-plugin-svgr';
+import { resolve } from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
+const getPlugins = () => {
+    return [
+        svgr(),
+        react(),
+        terser(),
+        cssInjectedByJsPlugin(),
+        {
+            ...rollupImportMapPlugin([
+                {
+                    imports: {
+                        react: 'https://www.nav.no/tms-min-side-assets/react/18/esm/index.js',
+                        'react-dom': 'https://www.nav.no/tms-min-side-assets/react-dom/18/esm/index.js',
+                    },
+                },
+            ]),
+            enforce: 'pre',
+            apply: 'build',
+        },
+    ];
+};
+
+const getConfig = () => ({
+    plugins: getPlugins(),
+    server: {
+        port: 3002,
+    },
+});
+
+// const getCdnConfig = () => ({
+//     plugins: getPlugins(),
+//     build: {
+//         manifest: true,
+//         copyPublicDir: false,
+//         rollupOptions: {
+//             input: {
+//                 app: resolve(__dirname, 'src/Mikrofrontend.tsx'),
+//             },
+//             preserveEntrySignatures: 'exports-only',
+//             output: {
+//                 entryFileNames: 'js/aia.[hash].js',
+//                 format: 'esm',
+//             },
+//         },
+//     },
+// });
+
+export default defineConfig(({ mode }) => {
+    if (mode === 'cdn') {
+        return getCdnConfig();
+    }
+    return getConfig();
+});
