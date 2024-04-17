f999e28cf6931b30a9f97e62db7fa0bae118c4c1
Jostein Holje <jostein.holje@nav.no>
2024-04-17 10:35:11 +0200

    Legger til språkstøtte

--- src/components/meldekort/meldekort-skjema.tsx
+++ src/components/meldekort/meldekort-skjema.tsx
@@ -1,12 +1,30 @@
 import { Alert, BodyLong, Box, Button, Checkbox, Heading, Radio, RadioGroup } from '@navikt/ds-react';
+import { lagHentTekstForSprak, Sprak } from '@navikt/arbeidssokerregisteret-utils';
 
 interface Props {
     visIkkeSvartAdvarsel: false | 'warning' | 'error';
+    sprak: Sprak;
 }
 
-const MeldekortSkjema = (props: Props) => {
-    const { visIkkeSvartAdvarsel } = props;
+const TEKSTER = {
+    nb: {
+        heading: 'Vil du fortsatt være arbeidssøker?',
+        infoText: 'Hvis du ønsker å være registrert som arbeidssøker hos NAV må du besvare disse spørsmålene.\n' +
+            'Informasjonen brukes i arbeidsmarkedsstatistikken.',
+        beenWorking: 'Har du vært i arbeid i perioden ',
+        yes: 'Ja',
+        no: 'Nei',
+        wantToBeRegistered: 'Jeg vil fortsatt være registrert som arbeidssøker',
+        submit: 'Send inn',
+        noReply: 'Du har ikke svart',
+        alertText1: 'Hvis du ikke svarer i løpet av ',
+        alertText2: ', vil du ikke lenger være registrert som arbeidssøker fra '
+    },
+};
 
+const MeldekortSkjema = (props: Props) => {
+    const { visIkkeSvartAdvarsel, sprak } = props;
+    const tekst = lagHentTekstForSprak(TEKSTER, sprak);
     return (
         <Box
             background="surface-default"
@@ -17,36 +35,29 @@ const MeldekortSkjema = (props: Props) => {
         >
             <div className={'pt-4 pb-3 px-5'}>
                 <Heading level="3" size="small">
-                    Vil du fortsatt være arbeidssøker?
+                    {tekst('heading')}
                 </Heading>
             </div>
             <div className={'py-4 px-6'}>
                 {visIkkeSvartAdvarsel && (
                     <Alert variant={visIkkeSvartAdvarsel} className={'mb-4'}>
-                        <Heading size={'xsmall'}>Du har ikke svart</Heading>
+                        <Heading size={'xsmall'}>{tekst('noReply')}</Heading>
                         <BodyLong>
-                            Hvis du ikke svarer i løpet av 20. april vil du ikke lenger være registrert som arbeidssøker
-                            fra 20. april
+                            {tekst('alertText1')} 20. april{tekst('alertText2')} 20. april
                         </BodyLong>
                     </Alert>
                 )}
-                <BodyLong spacing>
-                    Hvis du ønsker å være registrert som arbeidssøker hos NAV må du besvare disse spørsmålene.
-                    Informasjonen brukes i arbeidsmarkedsstatistikken.
-                </BodyLong>
-
+                <BodyLong spacing>{tekst('infoText')}</BodyLong>
                 <RadioGroup
-                    legend="Har du vært i arbeid i perioden 21.mars - 6. april?"
+                    legend={`${tekst('beenWorking')} 21.mars - 6. april?`}
                     onChange={(e) => console.log(e)}
                 >
-                    <Radio value="ja">Ja</Radio>
-                    <Radio value="nei">Nei</Radio>
+                    <Radio value="ja">{tekst('yes')}</Radio>
+                    <Radio value="nei">{tekst('no')}</Radio>
                 </RadioGroup>
 
-                <Checkbox value="bekreft">Jeg vil fortsatt være registrert som arbeidssøker</Checkbox>
-                <Button variant="primary" className={'mt-4'}>
-                    Send inn
-                </Button>
+                <Checkbox value="bekreft">{tekst('wantToBeRegistered')}</Checkbox>
+                <Button variant="primary" className={'mt-4'}>{tekst('submit')}</Button>
             </div>
         </Box>
     );
