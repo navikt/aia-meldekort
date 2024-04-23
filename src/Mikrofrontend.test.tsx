1161ced0bb6076104dc08c7f99a13ece3fc71b5b
Jostein Holje <jostein.holje@nav.no>
2024-04-23 11:56:32 +0200

    Endrer tekst på test

--- src/Mikrofrontend.test.tsx
+++ src/Mikrofrontend.test.tsx
@@ -1,12 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import Mikrofrontend from "./Mikrofrontend";
+import { render, screen } from '@testing-library/react';
+import Mikrofrontend from './Mikrofrontend';
 
 describe('Mikrofrontend', () => {
-  test("rendrer AiA som microfrontend", async () => {
-    render(<Mikrofrontend />);
+    test('rendrer Mikrofrontend', async () => {
+        render(<Mikrofrontend />);
 
-    await expect(
-        await screen.findByText("Vil du fortsatt være arbeidssøker?"),
-    ).toBeInTheDocument();
-  });
+        await expect(await screen.findByText('Vil du fortsatt være arbeidssøker?')).toBeInTheDocument();
+    });
 });
